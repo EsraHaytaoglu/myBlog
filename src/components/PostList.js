8f8e71503881da06274ff668cef6b41e16f4e0b1
Esra Cebi <esracebi.81@gmail.com>
2021-10-27 01:38:29 +0300

    zamana göre postları sıraladım

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,15 +2,19 @@ import React from "react";
 import Post from "./Post";
 import { useSelector } from "react-redux";
 import {  Box } from "@material-ui/core";
+import moment from "moment";
 
 function PostList() {
+  const convertRelativeTime = (date) => {
+    return moment(date).fromNow();
+  };
   const posts = useSelector((state) => state.posts.posts);
   return (
     <Box  sx={{ flexGrow: 1, overflow: 'hidden', px: 5, marginTop: 150}}>
       {posts.length > 0 && 
-        posts.map((post) => (
+        posts.sort((a, b) => convertRelativeTime(a.createdAt) > convertRelativeTime(b.createdAt) ? 1 : -1).map((post) => (
             <Post post={post}  key={post?._id}/>
-        ))}
+        )).sort()}
     </Box>
   );
 }
